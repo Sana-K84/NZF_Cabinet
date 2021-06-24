970ce258ea6251099a874b06c9a012091262934b
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2021-06-24 16:10:55 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
         function getInfo() {
             document.querySelector('.right').innerHTML =
 
-
-
                 '<div class="info"> <img class="info_img" src="img/foto/' + globArr.id + '.jpg" alt="">' +
 
                 '<div class="info_body"><span class="info_clue">П.І.Б.</span>' +
                 '<span class="info_data">' + globArr.name + '</span>' +
-
-
-
 
                 '<span class="info_clue">Цех</span>' +
                 '<span class="info_data">' + globArr.username + '</span>' +
@@ -24,13 +19,10 @@
                 '<span class="info_data">' + globArr.phone + '</span></div></div>'
         };
 
-
-
         function getVal() {
 
             let tab_numb = document.getElementById('tab_numb').value;
             let pass = document.getElementById('pass').value;
-
 
             async function getResponce() {
                 let response = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -39,11 +31,9 @@
                 let list = document.querySelector('.root');
                 list.innerHTML = " ";
 
-
                 for (let key in content) {
 
                     if (content[key].id == tab_numb && content[key].id == pass) {
-
 
                         var header = document.createElement('header');
                         header.classList = 'head';
@@ -67,36 +57,38 @@
                         getInfo();
                         break;
                     }
-
                 }
-
-
-
-
 
                 if (window.globArr == null) {
                     alert('Логин и пароль должны быть одним числом от 1 до 10');
                     location.reload();
                 };
-
-
             }
-
             getResponce();
-
         }
-
-
-
 
         function getPayroll() {
             document.querySelector('.right').innerHTML =
                 '<div class="payroll"><div class="payroll_head">Розрахунковий лист</div>' +
                 '<div class="select"><img src="img/Images/5.gif" class="select_img"><select><option>2020</option><option>2021</option></select><select><option>Січень</option><option>Лютий</option><option>Березень</option><option>Квітень</option><option>Травень</option><option>Червень</option><option>Липень</option><option>Серпень</option><option>Вересень</option><option>Жовтень</option><option>Листопад</option><option>Грудень</option></select></div><div id="print">Друк</div>' +
-
-                '<table border="0"><tr><td colspan="2">Нараховано</td><td>Утримано</td></tr><tr><td>' + globArr.email + '</td><td>' + globArr.address.zipcode + '</td><td>' + globArr.company.bs + '</td></tr><tr><td>' + globArr.address.street + '</td><td>' + globArr.website + '</td><td>' + globArr.address.suite + '</td></tr><tr><td colspan="2">Зарплата: ' + globArr.address.geo.lat + '</td><td>' + globArr.address.city + '</td> </tr><tr><td colspan="2">Аванс: ' + globArr.address.geo.lng + '</td>    <td></td></tr></div>'
-
-
+                '<div id="table"><table border="0"><tr><td colspan="2">Нараховано</td><td>Утримано</td></tr><tr><td>' + globArr.email + '</td><td>' + globArr.address.zipcode + '</td><td>' + globArr.company.bs + '</td></tr><tr><td>' + globArr.address.street + '</td><td>' + globArr.website + '</td><td>' + globArr.address.suite + '</td></tr><tr><td colspan="2">Зарплата: ' + globArr.address.geo.lat + '</td><td>' + globArr.address.city + '</td> </tr><tr><td colspan="2">Аванс: ' + globArr.address.geo.lng + '</td><td></td></tr></div></div>'
+            document.getElementById('print').onclick = function () {
+                function CallPrint() {
+                    var prtContent = document.getElementById('table');
+                    var prtCSS = '<link rel="stylesheet" href="src/style.css" type="text/css" />';
+                    var WinPrint = window.open('', '', 'left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
+                    WinPrint.document.write('<div id="table"');
+                    WinPrint.document.write(prtCSS);
+                    WinPrint.document.write(prtContent.innerHTML);
+                    WinPrint.document.write('</div>');
+                    WinPrint.document.close();
+                    WinPrint.focus();
+                    WinPrint.print();
+                    WinPrint.close();
+                    prtContent.innerHTML = strOldOne;
+                }
+                CallPrint();
+            }
         };
 
         function getEsc() {
